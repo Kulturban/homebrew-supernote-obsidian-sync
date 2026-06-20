b37d6489d9fc7ee908c6e218fc3e2f969e5fd231
Kulturban <275660186+Kulturban@users.noreply.github.com>
2026-06-20 16:44:32 +0200

    Update Supsidian cask to v0.9.3

--- Casks/supsidian.rb
+++ Casks/supsidian.rb
@@ -1,8 +1,8 @@
 cask "supsidian" do
-  version "0.9.2"
+  version "0.9.3"
 
   url "https://github.com/Kulturban/supernote-obsidian-sync/releases/download/v#{version}/Supsidian-v#{version}.zip"
-  sha256 "c962ed42134637f9baa5d6bdd5e5f557687e3e165d87c3811a982f4dce9c1e1b"
+  sha256 "1ea2d944d45e1dd844095b7be1ce25da04151fdb5c3825da4b8c460b59e6c30a"
 
   name "Supsidian"
   desc "Menu bar app for syncing Supernote notes to Obsidian"
